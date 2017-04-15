dd667ceca4fc13270a2d29e109fb972d17712e91
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-04-15 17:30:03 -0300

    Implementando o shell_sort.

--- shell_sort.js
+++ shell_sort.js
@@ -1,4 +1,16 @@
-function shell_sort(lista) {
+function shell_sort (lista) {
+    for (var h = lista.length; h > 0; h = parseInt(h / 2)) {
+        for (var i = h; i < lista.length; i++) {
+            var k = lista[i];
+            
+            for (var j = i; j >= h && k < lista[j - h]; j -= h) {
+                lista[j] = lista[j - h];
+            }
+
+            lista[j] = k;
+        }
+    }
+    
     return lista;
 }
 
