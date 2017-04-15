526545ff4d1bafb0f3f7df475427fab7042c3548
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-04-15 20:12:41 -0300

    Adicionando os arquivos de comparação.

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,4 +1,4 @@
-function insertion_sort(lista) {
+exports.insertion_sort = function(lista) {
     lista.forEach(function(valor, i) {
         while ((i > 0) && (lista[i-1] > valor)) {
             lista[i] = lista[i-1];
@@ -10,8 +10,3 @@ function insertion_sort(lista) {
 
     return lista;
 }
-
-// Teste da ordenação por inserção
-lista = [43, 22, 56, 7, 71, 54, 6, 44, 35, 4, 9, 34, 61, 26, 52, 10, 29, 75, 72, 23, 2, 95, 51, 90, 12, 20, 8, 80, 69, 50, 99, 59, 11, 5, 21, 33, 84, 88, 77, 24, 64, 47, 86, 41, 57, 17, 55, 91, 13, 30, 58, 42, 39, 46, 32, 31, 49, 74, 36, 82, 79, 100, 63, 73];
-resultado = insertion_sort(lista);
-console.log(resultado);
--- shell_sort.js
+++ shell_sort.js
@@ -1,4 +1,4 @@
-function shell_sort (lista) {
+exports.shell_sort = function(lista) {
     for (var h = lista.length; h > 0; h = parseInt(h / 2)) {
         for (var i = h; i < lista.length; i++) {
             var k = lista[i];
@@ -13,8 +13,3 @@ function shell_sort (lista) {
     
     return lista;
 }
-
-// Teste do algoritmo shell sort.
-lista = [43, 22, 56, 7, 71, 54, 6, 44, 35, 4, 9, 34, 61, 26, 52, 10, 29, 75, 72, 23, 2, 95, 51, 90, 12, 20, 8, 80, 69, 50, 99, 59, 11, 5, 21, 33, 84, 88, 77, 24, 64, 47, 86, 41, 57, 17, 55, 91, 13, 30, 58, 42, 39, 46, 32, 31, 49, 74, 36, 82, 79, 100, 63, 73];
-resultado = shell_sort(lista);
-console.log(resultado);
