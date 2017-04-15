da935b09a666a3c30abaf0f1385470af4f4dd2d8
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-04-15 16:56:35 -0300

    Implementando o algoritmo bubble sort.

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,8 +1,8 @@
-function shell_sort(lista) {
+function bubble_sort(lista) {
     return lista;
 }
 
-// Teste do algoritmo shell sort.
+// Teste do algoritmo bubble sort.
 lista = [43, 22, 56, 7, 71, 54, 6, 44, 35, 4, 9, 34, 61, 26, 52, 10, 29, 75, 72, 23, 2, 95, 51, 90, 12, 20, 8, 80, 69, 50, 99, 59, 11, 5, 21, 33, 84, 88, 77, 24, 64, 47, 86, 41, 57, 17, 55, 91, 13, 30, 58, 42, 39, 46, 32, 31, 49, 74, 36, 82, 79, 100, 63, 73];
-resultado = shell_sort(lista);
+resultado = bubble_sort(lista);
 console.log(resultado);
