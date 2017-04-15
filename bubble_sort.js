27334d20c3ad91b345809073a91f58301f5c0b03
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-04-15 17:05:27 -0300

    Implementado o algoritmo bubble sort.

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,4 +1,17 @@
 function bubble_sort(lista) {
+    var trocado;
+    do {
+        trocado = false;
+        for (var i = 0; i < lista.length - 1; i++) {
+            if (lista[i] > lista[i+1]) {
+                var aux = lista[i];
+                lista[i] = lista[i+1];
+                lista[i+1] = aux;
+                trocado = true;
+            }
+        }
+    } while (trocado);
+    
     return lista;
 }
 
