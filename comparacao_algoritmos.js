672778c527bd3133cee96aaa6f18a1356c2d5f46
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-05-25 08:58:40 -0300

    Implementando o algoritmo heap_sort.

--- comparacao_algoritmos.js
+++ comparacao_algoritmos.js
@@ -1,8 +1,14 @@
-// Importando os algoritmos de ordenação
+// Importando os algoritmos de ordenação menos eficientes.
 const bubble_sort = require('./bubble_sort');
 const insertion_sort = require('./insertion_sort');
 const selection_sort = require('./selection_sort');
 const shell_sort = require('./shell_sort');
+
+// Importando os algoritmos de ordenação mais eficientes.
+const heap_sort = require('./heap_sort');
+const quick_sort = require('./quick_sort');
+const merge_sort = require('./merge_sort');
+// const radix_sort = require('./radix_sort');
 
 // Importando e declarando as listas de valores utilizados pelos testes.
 const variaveis = require('./variaveis');
@@ -13,20 +19,24 @@ listas.forEach(function(lista, index) {
     console.log('\n');
     console.log('--- Iniciando os testes com a ' + parseInt(index+1) + 'ª lista que contém ' + lista.length + ' elementos.');
     
-    console.time('bubble_sort');
-    var resultado = bubble_sort.bubble_sort(lista);
-    console.timeEnd('bubble_sort');
+    // console.time('bubble_sort');
+    // var resultado = bubble_sort.bubble_sort(lista);
+    // console.timeEnd('bubble_sort');
 
-    console.time('insertion_sort');
-    var resultado = insertion_sort.insertion_sort(lista);
-    console.timeEnd('insertion_sort');
+    // console.time('insertion_sort');
+    // var resultado = insertion_sort.insertion_sort(lista);
+    // console.timeEnd('insertion_sort');
 
-    console.time('selection_sort');
-    resultado = selection_sort.selection_sort(lista);
-    console.timeEnd('selection_sort');
+    // console.time('selection_sort');
+    // resultado = selection_sort.selection_sort(lista);
+    // console.timeEnd('selection_sort');
 
-    console.time('shell_sort');
-    resultado = shell_sort.shell_sort(lista);
-    console.timeEnd('shell_sort');    
+    // console.time('shell_sort');
+    // resultado = shell_sort.shell_sort(lista);
+    // console.timeEnd('shell_sort');
+
+    console.time('heap_sort');
+    resultado = heap_sort.heap_sort(lista);
+    console.timeEnd('heap_sort');
 });
 console.log('\n');
--- heap_sort.js
+++ heap_sort.js
@@ -1,7 +1,46 @@
+function trocar_posicao(lista, i, j) {
+    var aux = lista[i];
+    lista[i] = lista[j];
+    lista[j] = aux;
+}
+
+function regenerar_arvore(lista, i, tamanho) {
+    while (true) {
+        var esquerda = i*2 + 1;
+        var direita = i*2 + 2;
+        var maior_valor = i;
+
+        if (esquerda < tamanho && lista[esquerda] > lista[maior_valor]) {
+            maior_valor = esquerda;
+        }
+
+        if (direita < tamanho && lista[direita] > lista[maior_valor]) {
+            maior_valor = direita;
+        }
+
+        if (i == maior_valor) {
+            break;
+        }
+
+        trocar_posicao(lista, i, maior_valor);
+        i = maior_valor;
+    }
+}
+
+function ordenar(lista, tamanho) {
+    for (var i = Math.floor(tamanho/2); i >= 0; i--) {
+        regenerar_arvore(lista, i, tamanho);
+    }
+}
+
 exports.heap_sort = function(lista) {
     lista_ordenada = Array.from(lista);
-    
-    // TODO: Implementar.
+    ordenar(lista_ordenada, lista_ordenada.length);
+
+    for (var i = lista_ordenada.length - 1; i > 0; i--) {
+        trocar_posicao(lista_ordenada, i, 0);
+        regenerar_arvore(lista_ordenada, 0, i-1);
+    }
 
     return lista_ordenada;
 }
